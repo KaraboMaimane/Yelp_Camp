c51c3c8231d9005eac03779c3106eec67af050bc
karabo <kmaimane@mweb.com>
2020-09-24 00:17:20 +0200

    Catering for the comment creation and display

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get('/campgrounds', (request, response) => {
         } else {
             campgrounds = campground;
             console.log(campground);
-            response.render('campgrounds', {campgrounds: campgrounds});
+            response.render('campgrounds/campgrounds', {campgrounds: campgrounds});
         }
     })
 })
@@ -50,19 +50,55 @@ app.post('/campgrounds', (request, response) => {
 })
 
 app.get('/campgrounds/new', (request, response) => {
-    response.render('new');
+    response.render('campgrounds/new');
 })
 
 app.get('/campgrounds/:id', (request, response) => {
     Campground.findById(request.params.id).populate("comments").exec((error, foundCampground) => {
         if(!error){
-            response.render('view-campground',  {campground: foundCampground});
+            response.render('campgrounds/view-campground',  {campground: foundCampground});
         } else {
             response.response.redirect('/campgrounds');
         }
     })
 })
 
+// =======================================================================================================================================
+// Comments Routes
+// =======================================================================================================================================
+app.get('/campgrounds/:id/comments/new', (request, response) => {
+    // find campground by id
+    Campground.findById(request.params.id, (error, campground) => {
+        if(!error){
+            response.render('comments/new', {campground: campground})
+        } else {
+            console.log(error);
+        }
+    })
+})
+
+app.post('/campgrounds/:id/comments', (request, response) => {
+    //lookup campground using id
+    Campground.findById(request.params.id, (error, campground) => {
+        if(!error){
+            //create comment
+            Comment.create(request.body.comment, (error, comment) => {
+                if(!error){
+                    // save the comment to the campgrounds object
+                    campground.comments.push(comment);
+                    campground.save();
+                    response.redirect(`/campgrounds/${campground._id}`);
+                } else {
+                    console.log(error);
+                }
+            });
+        } else {
+            console.log(error);
+            response.redirect('/campgrounds')
+        }
+    })
+})
+
 app.listen('2000', () => {
     console.log('YelpCamp is live on port:2000');
-})
+});
